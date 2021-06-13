5fcd45e8ad5cebf82a08fe9929834780288d56a4
SkillVirus <skillvirus@kakao.com>
2021-06-13 08:04:59 +0000

    Stack 예제 보완 완료

--- src/014.DataStructure/02.Stack.c
+++ src/014.DataStructure/02.Stack.c
@@ -4,9 +4,9 @@
 
 int stack[MAX_STACK_SIZE];
 int top = -1;
- 
-int IsEmpty(){
-    if(top < 0) {
+
+int IsEmpty() {
+    if (top < 0) {
         return true;
     }
     else {
@@ -14,7 +14,7 @@ int IsEmpty(){
     }
 }
 
-int IsFull(){
+int IsFull() {
     int temp = top;
     if (++temp >= MAX_STACK_SIZE) {
         return true;
@@ -23,61 +23,68 @@ int IsFull(){
         return false;
     }
 }
- 
-void push(int value){
-    if(IsFull() == true) {
+
+void push(int value) {
+    if (IsFull() == true) {
         printf("Stack is Full. \n");
     }
     else {
-        stack[++top] = value; 
+        stack[++top] = value;
     }
 }
- 
-int pop(){
+
+int pop() {
     if (IsEmpty() == true) {
         printf("Stack is empty \n");
-    } 
+    }
     else {
         int currentIndex = top;
         int returnValue = stack[top--];
         stack[currentIndex] = 0;
-        return returnValue;
+        printf("%d \n", returnValue);
+        //return returnValue;
     }
 }
- 
-int main(){
-    
+
+int main() {
+
     push(3);
     push(2);
     push(10);
     push(12);
     push(13);
     //push(14);
-    
+
     printf("-----Stack의 POP 전----- \n");
     printf("배열5 : %d \n", stack[4]);
     printf("배열4 : %d \n", stack[3]);
     printf("배열3 : %d \n", stack[2]);
     printf("배열2 : %d \n", stack[1]);
     printf("배열1 : %d \n", stack[0]);
-    
+
     printf("\n");
+
+    pop();
+    pop();
+    pop();
+    pop();
+    //pop();
     
-    printf("%d \n", pop());
-    printf("%d \n", pop());
-    printf("%d \n", pop());
-    printf("%d \n", pop());
-    printf("%d \n", pop());
-    printf("%d \n", pop());
-    
+    //printf("%d \n", pop());
+    //printf("%d \n", pop());
+    //printf("%d \n", pop());
+    //printf("%d \n", pop());
+    //printf("%d \n", pop());
+    //printf("%d \n", pop());
+
     printf("\n");
-    
+
     printf("-----Stack의 POP 후----- \n");
     printf("배열5 : %d \n", stack[4]);
     printf("배열4 : %d \n", stack[3]);
     printf("배열3 : %d \n", stack[2]);
     printf("배열2 : %d \n", stack[1]);
     printf("배열1 : %d \n", stack[0]);
- 
+
     return 0;
 }
